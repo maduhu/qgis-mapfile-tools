f7a396e8aed9817ccef35ba40758dbcfc5135c0d
Vincent Picavet <vincent.picavet@oslandia.com>
2013-03-15 11:22:57 +0100

    Polish a bit the editor.

--- mapfile_tools/simple_map_editor.py
+++ mapfile_tools/simple_map_editor.py
@@ -23,22 +23,19 @@ class SimpleMapEditor(QsciScintilla):
         font = QFont()
         font.setFamily('Courier')
         font.setFixedPitch(True)
-        font.setPointSize(10)
+        font.setPointSize(8)
         self.setFont(font)
         self.setMarginsFont(font)
 
         # Margin 0 is used for line numbers
         fontmetrics = QFontMetrics(font)
         self.setMarginsFont(font)
-        self.setMarginWidth(0, fontmetrics.width("00000") + 6)
+        self.setMarginWidth(0, fontmetrics.width("000") + 6)
         self.setMarginLineNumbers(0, True)
         self.setMarginsBackgroundColor(QColor("#cccccc"))
 
         # Clickable margin 1 for showing markers
         self.setMarginSensitivity(1, True)
-        self.connect(self,
-            SIGNAL('marginClicked(int, int, Qt::KeyboardModifiers)'),
-            self.on_margin_clicked)
         self.markerDefine(QsciScintilla.RightArrow,
             self.ARROW_MARKER_NUM)
         self.setMarkerBackgroundColor(QColor("#ee1111"),
@@ -58,14 +55,12 @@ class SimpleMapEditor(QsciScintilla):
         # here: http://www.scintilla.org/ScintillaDoc.html)
         self.SendScintilla(QsciScintilla.SCI_SETHSCROLLBAR, 0)
 
-        # not too small
-        self.setMinimumSize(300, 300)
+        # min size
+        self.setMinimumSize(40, 40)
 
-	# Add MapFile Custom Lexer
+        # Add MapFile Custom Lexer
         self.lexer = MapLexer(self)
         self.setLexer(self.lexer)
-
- 
 
     def load(self, filename):
         """Load file into editor."""
@@ -74,11 +69,4 @@ class SimpleMapEditor(QsciScintilla):
 
     def getText(self):
         return self.text()
-
-    def on_margin_clicked(self, nmargin, nline, modifiers):
-        # Toggle marker for the line the margin was clicked on
-        if self.markersAtLine(nline) != 0:
-            self.markerDelete(nline, self.ARROW_MARKER_NUM)
-        else:
-            self.markerAdd(nline, self.ARROW_MARKER_NUM)
 
