1c4e0ec469b5419ccd41c733875b04b1b48b7fbc
Vincent Picavet <vincent.picavet@oslandia.com>
2013-03-16 14:46:29 +0100

    Comment debug.

--- mapfile_tools/maplexer.py
+++ mapfile_tools/maplexer.py
@@ -94,19 +94,19 @@ class MapLexer(Qsci.QsciLexerCustom):
 
                  elif state == LEX_STATE_IN_SINGLE_QUOTES and chr(c) == '\'':
                     state = LEX_STATE_INIT
-                    print 'VALUE: %s' % token 
+                    # print 'VALUE: %s' % token 
                     set_style(len(token), self.Value)
                     token = '' 
 
                  elif state == LEX_STATE_IN_DOUBLE_QUOTES and chr(c) == '"':
                     state = LEX_STATE_INIT
-                    print 'VALUE: %s' % token 
+                    # print 'VALUE: %s' % token 
                     set_style(len(token), self.Value)
                     token = '' 
 
                  elif state == LEX_STATE_COMMENT and chr(c) == '\n':
                     state = LEX_STATE_INIT
-                    print 'COMMENT: %s' % token 
+                    # print 'COMMENT: %s' % token 
                     set_style(len(token), self.Comment)
                     token = '' 
 
@@ -116,7 +116,7 @@ class MapLexer(Qsci.QsciLexerCustom):
                           pos = token.find(i) 
                           if pos >= 0:
                               set_style(len(token), self.Object)
-                              print 'OBJECT: %s' % token 
+                              # print 'OBJECT: %s' % token 
                               token = '' 
 			      break
 		    dict = ['ALIGN', 'ALPHACOLOR', 'ANCHORPOINT', 'ANGLE', 'BACKGROUNDCOLOR', 'BUFFER', 'CENTER', 'CHARACTER', 'CLASSGROUP', 'CLASSITEM', 'OUTLINECOLOR', 'COLOR', 'COLORRANGE', 'CONFIG', 'CONNECTION', 'CONNECTIONTYPE', 'DATA', 'DATAPATTERN', 'DATARANGE', 'DEBUG', 'DEFRESOLUTION', 'DRIVER', 'EMPTY', 'ENCODING', 'ERROR', 'EXPRESSION', 'EXTENSION', 'EXTENT', 'FILLED', 'FILTER', 'FILTERITEM', 'FONT', 'FONTSET', 'FOOTER', 'FORCE', 'FORMATOPTION', 'FROM', 'GAP', 'GEOMTRANSFORM', 'GRATICULE', 'GRID', 'GRIDSTEP', 'GROUP', 'HEADER', 'IMAGECOLOR', 'IMAGEMODE', 'IMAGEPATH', 'IMAGEQUALITY', 'IMAGETYPE', 'IMAGEURL', 'IMAGE', 'INCLUDE', 'INDEX', 'INITIALGAP', 'INTERVALS', 'ITEMS', 'KEYIMAGE', 'KEYSIZE', 'KEYSPACING', 'LABELCACHE_MAP_EDGE_BUFFER', 'LABELCACHE', 'LABELFORMAT', 'LABELITEM', 'LABELMAXSCALEDENOM', 'LABELMINSCALEDENOM', 'LABELREQUIRES', 'LABELSIZEITEM', 'LATLON', 'LINECAP', 'LINEJOIN', 'LINEJOINMAXSIZE', 'LOG', 'MARKER', 'MARKERSIZE', 'MASK', 'MAXARCS', 'MAXBOXSIZE', 'MAXDISTANCE', 'MAXFEATURES', 'MAXINTERVAL', 'MAXLENGTH', 'MAXOVERLAPANGLE', 'MAXSCALE', 'MAXSCALEDENOM', 'MAXSIZE', 'MAXSUBDIVIDE', 'MAXTEMPLATE', 'MAXWIDTH', 'MIMETYPE', 'MINARCS', 'MINBOXSIZE', 'MINDISTANCE', 'MINFEATURESIZE', 'MININTERVAL', 'MINSCALE', 'MINSCALEDENOM', 'MINSIZE', 'MINSUBDIVIDE', 'MINTEMPLATE', 'MINWIDTH', 'NAME', 'OFFSET', 'OFFSITE', 'OPACITY', 'OUTLINEWIDTH', 'PARTIALS', 'POLAROFFSET', 'POSITION', 'POSTLABELCACHE', 'PRIORITY', 'PROCESSING', 'QUERYFORMAT', 'REPEATDISTANCE', 'REQUIRES', 'RESOLUTION', 'SCALE', 'SHADOWCOLOR', 'SHADOWSIZE', 'SHAPEPATH', 'SIZE', 'SIZEUNITS', 'STATUS', 'STYLEITEM', 'SYMBOLSCALE', 'SYMBOLSCALEDENOM', 'SYMBOLSET', 'TABLE', 'TEMPLATE', 'TEMPLATEPATTERN', 'TEXT', 'TILEINDEX', 'TILEITEM', 'TITLE', 'TO', 'TOLERANCE', 'TOLERANCEUNITS', 'TRANSFORM', 'TRANSPARENT', 'TYPE', 'UNITS', 'WIDTH', 'WRAP']
@@ -124,9 +124,9 @@ class MapLexer(Qsci.QsciLexerCustom):
                           pos = token.find(i) 
                           if pos >= 0:
                               set_style(len(token), self.Parameter)
-                              print 'Parameter: %s' % token 
+                              # print 'Parameter: %s' % token 
                               token = '' 
 			      break
                     set_style(len(token), self.Default)
-                    print 'DEFAULT: %s' % token 
+                    # print 'DEFAULT: %s' % token 
                     token = '' 
